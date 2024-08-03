634a158043a32c0cc1871c2185ad43fb856f2ad6
Ken Tsui <kenhktsui@gmail.com>
2024-08-03 18:55:55 +0100

    feat: prompt validation

--- anyclassifier/annotation/prompt.py
+++ anyclassifier/annotation/prompt.py
@@ -21,6 +21,15 @@ class AnnotationPrompt:
     few_shot_examples: Optional[List[Example]] = None
 
     def get_prompt(self, text: str):
+        label_name = [l.name for l in self.label_definition]
+        assert len(set(label_name)) == len(self.label_definition), "The name of label has to be unique."
+        assert len(set(l.desc for l in self.label_definition)) == len(
+            self.label_definition), "The desc of label has to be unique."
+
+        if self.few_shot_examples is not None:
+            for fs in self.few_shot_examples:
+                assert fs.label in label_name, f"The few shot example is not matching the label: {fs.label}"
+
         label_defn_str = "\n".join([f"{ld.name}: {ld.desc}" for ld in self.label_definition])
         example_str = "" if self.few_shot_examples is None else "\nHere are some examples:\n" + "\n".join(
             [f"Example {i+1}.\nText: {fse.text}\nLabel: {fse.label}" for i, fse in enumerate(self.few_shot_examples)]
@@ -48,8 +57,8 @@ if __name__ == "__main__":
             Label(name="0", desc='negative sentiment')
         ],
         few_shot_examples=[
-            Example(text="This is good movie", label="positive"),
-            Example(text="It is a waste of time.", label="negative")
+            Example(text="This is good movie", label="1"),
+            Example(text="It is a waste of time.", label="0")
         ]
     )
     print(prompt.get_prompt("It is one of best I ever watched."))
